5cb3759b131855b12a49a1c2e2d4eb30ceb9e22a
yoonseonjin <saltycreamlatte@gmail.com>
2023-06-01 23:43:58 +0900

    230601(Thu)

---
+++ SQL 연습문제/Lv. 1/SELECT/상위 n개 레코드.sql
@@ -0,0 +1,3 @@
+SELECT NAME FROM ANIMAL_INS ORDER BY DATETIME LIMIT 1;
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/59405
---
+++ SQL 연습문제/Lv. 1/SELECT/인기있는 아이스크림.sql
@@ -0,0 +1,3 @@
+SELECT FLAVOR FROM FIRST_HALF ORDER BY TOTAL_ORDER DESC, SHIPMENT_ID;
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/133024
