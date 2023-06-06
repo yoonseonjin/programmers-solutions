10608f5638b4f8e9254464f57f369938bd84ce02
yoonseonjin <saltycreamlatte@gmail.com>
2023-06-07 03:55:07 +0900

    Code refactoring

--- 코딩테스트 연습문제/Lv. 1/가운데 글자 가져오기.java
+++ 코딩테스트 연습문제/Lv. 1/가운데 글자 가져오기.java
@@ -2,14 +2,13 @@ class Solution {
     public String solution(String s) {
         String[] answer = s.split("");
         int len = answer.length;
-        
+
         if (len % 2 == 0) {
             int midIndex = (len / 2);
             return answer[midIndex - 1] + answer[midIndex];
-        } else {
-            int midIndex = ((len + 1) / 2) - 1;
-            return answer[midIndex];
         }
+        int midIndex = ((len + 1) / 2) - 1;
+        return answer[midIndex];
     }
 }
 
--- 코딩테스트 연습문제/Lv. 1/음양 더하기(If Statements).java
+++ 코딩테스트 연습문제/Lv. 1/음양 더하기(If Statements).java
@@ -1,16 +1,18 @@
 class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
-        
+
         for (int i = 0; i < absolutes.length; i++) {
             if (signs[i] == true) {
                 answer += absolutes[i];
-            } else {
+            }
+            if (signs[i] == false) {
                 answer -= absolutes[i];
             }
         }
         return answer;
     }
 }
+
 
 // https://school.programmers.co.kr/learn/courses/30/lessons/76501
