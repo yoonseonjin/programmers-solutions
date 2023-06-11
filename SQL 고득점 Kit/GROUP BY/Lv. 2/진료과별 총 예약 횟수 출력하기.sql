6ef5560bae19f88e72f43f0304f72d83be6c562f
yoonseonjin <saltycreamlatte@gmail.com>
2023-06-12 02:30:50 +0900

    230611(Sun)

---
+++ SQL 고득점 Kit/GROUP BY/Lv. 2/진료과별 총 예약 횟수 출력하기.sql
@@ -0,0 +1,8 @@
+SELECT MCDP_CD AS '진료과코드',
+       COUNT(MCDP_CD) AS '5월예약건수'
+FROM APPOINTMENT
+WHERE YEAR(APNT_YMD) = 2022 AND MONTH(APNT_YMD) = 5
+GROUP BY MCDP_CD
+ORDER BY COUNT(MCDP_CD), MCDP_CD;
+
+-- https://school.programmers.co.kr/learn/courses/30/lessons/132202
---
+++ 코딩 기초 트레이닝/리스트(배열)/n 번째 원소까지.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+        return Arrays.copyOf(num_list, n);
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181889
