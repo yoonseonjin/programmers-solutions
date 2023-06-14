b12e8e9feaa3082d2d117fdda2ac1a0cd14389bf
yoonseonjin <saltycreamlatte@gmail.com>
2023-06-15 01:40:11 +0900

    230614(Wed)

---
+++ 코딩 기초 트레이닝/문자열/접두사인지 확인하기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String my_string, String is_prefix) {
+        return my_string.startsWith(is_prefix) ? 1 : 0;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181906
---
+++ 코딩 기초 트레이닝/조건문 활용/부분 문자열.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(String str1, String str2) {
+        return str2.contains(str1) ? 1 : 0;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181842
