6d2f39701ed778be236d0f0612457fa13ade83eb
yoonseonjin <saltycreamlatte@gmail.com>
2023-07-05 00:02:00 +0900

    Update 최대공약수와 최소공배수.java

--- 코딩테스트 연습문제/Lv. 1/최대공약수와 최소공배수.java
+++ 코딩테스트 연습문제/Lv. 1/최대공약수와 최소공배수.java
@@ -2,13 +2,13 @@ class Solution {
     public int[] solution(int n, int m) {
         int[] answer = new int[2];
         int min = (n < m) ? n : m;
-        
-		for (int i = 1; i <= min; i++) {
-			if (n % i == 0 && m % i == 0) {
+
+        for (int i = 1; i <= min; i++) {
+            if (n % i == 0 && m % i == 0) {
                 answer[0] = i;
                 answer[1] = (n * m) / i;
             }  
-		}
+        }
         return answer;
     }
 }
