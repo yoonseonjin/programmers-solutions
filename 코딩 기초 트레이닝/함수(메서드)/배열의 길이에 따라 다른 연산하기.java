c5619c997fe56a5ecb156eb312b004ef21a208e1
yoonseonjin <saltycreamlatte@gmail.com>
2023-07-10 17:36:33 +0900

    230710(Mon)

---
+++ 코딩 기초 트레이닝/함수(메서드)/배열의 길이에 따라 다른 연산하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int n) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr.length % 2 == 1 && i % 2 == 0) {
+                arr[i] += n;
+            }
+            if (arr.length % 2 == 0 && i % 2 == 1) {
+                arr[i] += n;
+            }
+        }
+        return arr;
+    }
+}
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/181854
